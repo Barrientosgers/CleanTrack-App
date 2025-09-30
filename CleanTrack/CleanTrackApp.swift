b2cf0dd4d69aff5be36fccefd789f34bd40edab4
Gerson Barrientos <gersonbarrientos62@gmail.com>
2025-09-30 12:03:24 -0700

    Initial Commit

---
+++ CleanTrack/CleanTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanTrackApp.swift
+//  CleanTrack
+//
+//  Created by Gerson Barrientos on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
